a454e5645811f68575ea2b27a03b058f245e1d24
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-05 13:43:56 -0300

    refactor: Refatora aplicando o 'S' de Solid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,8 @@ public class Main {
 		Order o = new Order();
 		o.add(new Item("Café", 8.0, 2)); // 16
 		o.add(new Item("Bolo", 12.5, 1)); // 12.5 -> subtotal 28.5
+		System.setProperty("tax.rate", "0.08");
 		String receipt = new ReceiptService().generate(o);
 		System.out.println(receipt);
-		System.out.println("Push teste para testar conexão com o repositório");
-		
 	}
 }
--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -2,18 +2,17 @@ package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
 	public String generate(Order order) {
+		ReceiptFormatter receipt = new ReceiptFormatter();
+		TaxCalculator calc = new TaxCalculator(Double.parseDouble(System.getProperty("tax.rate")));
 		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
-		double total = subtotal + tax;
+		double calculatedTax = calc.calculate(subtotal); //Taxa 10 fixa :(
+		double total = subtotal + calculatedTax;
+
 		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
-		sb.append("=== RECIBO ===\n");
 		for (var i : order.getItems()) {
 			sb.append(i.getName()).append(" x").append(i.getQuantity()).append(" = ").append(i.getUnitPrice() * i.getQuantity())
 					.append("\n");
 		}
-		sb.append("Subtotal: ").append(subtotal).append("\n");
-		sb.append("Tax: ").append(tax).append("\n");
-		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
+		return receipt.returnReceipt(sb.toString(), subtotal, calc.getTaxValue(), total);
 	}
 }
